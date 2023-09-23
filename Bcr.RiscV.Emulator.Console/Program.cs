de3cc3cc0ccccbacfed35314212be42aca8fdf8d
Blake Ramsdell <blaker@gmail.com>
2023-09-23 11:55:51 -0700

    Make console a class

--- Bcr.RiscV.Emulator.Console/Program.cs
+++ Bcr.RiscV.Emulator.Console/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Bcr.RiscV.Emulator.Console;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello, World!");
+    }
+}
