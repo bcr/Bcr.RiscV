8dd27fa4b17182f3f2636e618a89f756792b6168
Blake Ramsdell <blaker@gmail.com>
2023-09-23 13:05:58 -0700

    Add ELF loading

--- Bcr.RiscV.Emulator.Console/Program.cs
+++ Bcr.RiscV.Emulator.Console/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace Bcr.RiscV.Emulator.Console;
+﻿using ELFSharp.ELF;
+
+namespace Bcr.RiscV.Emulator.Console;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        System.Console.WriteLine("Hello, World!");
+        string? filename = null;
+        foreach (var arg in args)
+        {
+            if (!arg.StartsWith("-"))
+            {
+                // Filename
+                filename = arg;
+            }
+            else
+            {
+                // Option of some sort
+            }
+        }
+        // Load ELF file
+        System.Console.WriteLine($"Loading {filename}");
+        var elf = ELFReader.Load(filename);
+        foreach(var header in elf.Sections)
+        {
+            System.Console.WriteLine(header);
+        }
     }
 }
