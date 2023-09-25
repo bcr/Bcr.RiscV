81c0ab3df6d8d23308792f8a0b38ec89d06b45ec
Blake Ramsdell <blaker@gmail.com>
2023-09-24 23:08:45 -0700

    Add JAL instruction

--- Bcr.RiscV.Emulator.Console/Emulator.cs
+++ Bcr.RiscV.Emulator.Console/Emulator.cs
@@ -17,11 +17,81 @@ class Emulator : IEmulator
 
     public void Run()
     {
-        var IP = startAddress;
+        var PC = startAddress;
+        var registers = new uint[32];
         _logger.LogInformation("Emulator starting");
-        // Read next instruction
-        var instruction = _memory.ReadInstruction(IP);
-        // Execute instruction
-        // Adjust PC if required
+        while (true)
+        {
+            // Read next instruction
+            var instruction = _memory.ReadInstruction(PC);
+            _logger.LogInformation("Instruction is {instruction:X8}", instruction);
+            var opcode = instruction & 0b111_1111;
+            var rd = (instruction & 0b1111_1000_0000) >> 7;
+            bool pcNeedsAdjusting = true;
+            switch (opcode)
+            {
+                case 0b110_1111:
+                    // JAL
+                    _logger.LogInformation("{rd}", rd);
+                    registers[rd] = PC + 4;
+                    // Compute immediate
+                    var offset = UJComputeImmediate(instruction);
+                    // Add immediate to PC
+                    PC = (offset < 0) ? (PC - (uint) (-offset)) : (PC + (uint) offset);
+                    pcNeedsAdjusting = false;
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+            // Let people write to x0 but then fix it
+            registers[0] = 0;
+            // Execute instruction
+            // Adjust PC if required
+            if (pcNeedsAdjusting)
+            {
+                PC += 4;
+            }
+        }
+    }
+
+    private int SignExtend(int value, int bitPosition)
+    {
+        var finalValue = value;
+
+        if ((value & (1 << bitPosition)) != 0)
+        {
+            var oneBits = 0xFFFF_FFFF << (bitPosition + 1);
+            finalValue |= (int) oneBits;
+        }
+        return finalValue;
+    }
+
+    private int UJComputeImmediate(uint instruction)
+    {
+        // imm[20|10:1|11|19:12]
+        Range[] ranges = {
+            new System.Range(20, 20),
+            new System.Range(10, 1),
+            new System.Range(11, 11),
+            new System.Range(19, 12),
+        };
+        int offset = 31;
+        int finalValue = 0;
+
+        foreach (var range in ranges)
+        {
+            var rangeLength = range.Start.Value - range.End.Value + 1;
+
+            var mask = (1 << rangeLength) - 1;
+            mask <<= offset - (rangeLength - 1);
+            var value = instruction & mask;
+            value >>= (offset - range.Start.Value);
+            finalValue |= (int) value;
+
+            offset -= rangeLength;
+        }
+
+        finalValue = SignExtend(finalValue, 20);
+        return finalValue;
     }
 }
