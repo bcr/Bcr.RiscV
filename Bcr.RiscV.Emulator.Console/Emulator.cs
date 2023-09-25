434334b876382900bb739969037a0bcf09ee3b34
Blake Ramsdell <blaker@gmail.com>
2023-09-25 02:18:56 -0700

    Implement BEQ instruction

--- Bcr.RiscV.Emulator.Console/Emulator.cs
+++ Bcr.RiscV.Emulator.Console/Emulator.cs
@@ -86,6 +86,10 @@ class Emulator : IEmulator
                     // Branch
                     switch (funct3)
                     {
+                        case 0b000:
+                            // BEQ
+                            conditionMet = registers[rs1] == registers[rs2];
+                            break;
                         case 0b001:
                             // BNE
                             conditionMet = registers[rs1] != registers[rs2];
